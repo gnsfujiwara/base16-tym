91b3877e49b428d2c4210fed25f9d1797b486625
gnsfujiwara <gnsfujiwara@users.noreply.github.com>
2023-03-18 00:40:17 +0000

    Update with the latest base16-project colorschemes

---
+++ themes/base16-mountain.lua
@@ -0,0 +1,50 @@
+--[[
+
+Base16 Mountain
+Scheme author: gnsfujiwara (https://github.com/gnsfujiwara) based on Mountain Theme (https://github.com/mountain-theme/Mountain)
+Template author: gnsfujiwara (https://github.com/gnsfujiwara)
+
+--]]
+
+return {
+  -- Special
+  color_foreground = '#f0f0f0',
+  color_background = '#0f0f0f',
+  color_window_background = '#0f0f0f',
+  color_bold = '#f0f0f0',
+  color_cursor = '#f0f0f0',
+  color_cursor_foreground = '#0f0f0f',
+  color_highlight = '#262626',
+
+  -- Black
+  color_0 = '#0f0f0f',
+  color_8 = '#4c4c4c',
+
+  -- Red
+  color_1 = '#ac8a8c',
+  color_9 = '#ac8a8c',
+
+  -- Green
+  color_2 = '#8aac8b',
+  color_10 = '#8aac8b',
+
+  -- Yellow
+  color_3 = '#aca98a',
+  color_11 = '#aca98a',
+
+  -- Blue
+  color_4 = '#8f8aac',
+  color_12 = '#8f8aac',
+
+  -- Magenta
+  color_5 = '#ac8aac',
+  color_13 = '#ac8aac',
+
+  -- Cyan
+  color_6 = '#8aabac',
+  color_14 = '#8aabac',
+
+  -- White
+  color_7 = '#f0f0f0',
+  color_15 = '#cacaca',
+}
