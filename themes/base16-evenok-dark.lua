0c97e7cbd6a4b0aebfda69d75edc7eaa640932c8
gnsfujiwara <gnsfujiwara@users.noreply.github.com>
2023-05-07 01:22:00 +0000

    Update with the latest base16-project colorschemes

---
+++ themes/base16-evenok-dark.lua
@@ -0,0 +1,49 @@
+--[[
+
+Base16 Evenok Dark
+Scheme author: Mekeor Melire
+Template author: gnsfujiwara (https://github.com/gnsfujiwara)
+
+--]]
+
+return {
+  -- Special
+  color_foreground = '#d0d0d0',
+  color_background = '#000000',
+  color_window_background = '#000000',
+  color_bold = '#d0d0d0',
+  color_cursor = '#d0d0d0',
+  color_cursor_foreground = '#000000',
+
+  -- Black
+  color_0 = '#000000',
+  color_8 = '#505050',
+
+  -- Red
+  color_1 = '#f5708a',
+  color_9 = '#f5708a',
+
+  -- Green
+  color_2 = '#54bc5c',
+  color_10 = '#54bc5c',
+
+  -- Yellow
+  color_3 = '#b8a300',
+  color_11 = '#b8a300',
+
+  -- Blue
+  color_4 = '#00aff2',
+  color_12 = '#00aff2',
+
+  -- Magenta
+  color_5 = '#9095ff',
+  color_13 = '#9095ff',
+
+  -- Cyan
+  color_6 = '#00bab3',
+  color_14 = '#00bab3',
+
+  -- White
+  color_7 = '#d0d0d0',
+  color_15 = '#ffffff',
+}
